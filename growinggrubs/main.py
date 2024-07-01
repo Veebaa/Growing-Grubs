6084121831105a2b56d0f98fa91264175cdd3f23
Veritia <funkyvish88@gmail.com>
2024-07-01 11:45:37 +0100

    update

--- growinggrubs/main.py
+++ growinggrubs/main.py
@@ -1,11 +1,24 @@
 import os
 import json
+import sqlalchemy
 
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+
 
 from database import db, User
 
 app = Flask(__name__)
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "growinggrubs.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
